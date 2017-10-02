1adc340ffc3734d70b4a86f589a3d9459ce0e2d1
Collin Lyons <collinclyons@gmail.com>
2017-10-01 21:55:27 -0700

    Imported some cool models

---
+++ Assets/Scripts/Level/LevelInitializer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace LevelControl
+{
+    public class LevelInitializer : MonoBehaviour
+    {
+
+        public GameObject gridBasePrefab;
+
+        public int unitCount = 1;
+        public GameObject unitPrefabs;
+
+        private WaitForEndOfFrame waitEF;
+
+        public GridStats gridStats;
+
+        [System.Serializable]
+        public class GridStats
+        {
+            int maxX = 10;
+            int maxY = 3;
+            int maxZ = 10;
+
+            public float offsetX = 1;
+            public float offsetY = 1;
+            public float offsetZ = 1;
+        }
+    }
+}
